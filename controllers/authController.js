5d4948a320223d44068b30b56ba8c41d2f65a4cb
Alexandr Barbashev <barbashev.2015@gmail.com>
2025-05-18 18:14:22 +0300

    Add signup, JWT

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+export const signup = catchAsync(async (req, res) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a vaalid email'],
+  },
+  photo: {
+    type: String,
+    default:
+      'https://res.cloudinary.com/dqj0xg3zv/image/upload/v1698231234/default.png',
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Password confirmation is required'],
+    validate: {
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: 'Passwords do not match',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined; // Remove passwordConfirm from the document
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
